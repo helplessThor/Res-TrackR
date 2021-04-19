62f1cf943568ce007a8f75df2e996901322fd5c6
Kuntal Paul <66440538+helplessThor@users.noreply.github.com>
2021-04-19 18:38:55 +0530

    Add files via upload

---
+++ touch_gps/touch_gps.ino
@@ -0,0 +1,55 @@
+//TrackR
+//Algo: Fetch-Trigger-Respond
+
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+int state = 0;
+const int pin = 7;
+float gpslat, gpslon;
+
+TinyGPS gps;
+SoftwareSerial sgps(2, 3);
+SoftwareSerial sgsm(9, 10);
+
+void setup()
+{
+  sgsm.begin(9600);
+  sgps.begin(9600);
+}
+
+void loop()
+{
+  sgps.listen();
+  while (sgps.available())
+  {
+    int c = sgps.read();
+    if (gps.encode(c))
+    {
+      gps.f_get_position(&gpslat, &gpslon);
+    }
+  }
+    if (digitalRead(pin) == HIGH && state == 0) {
+      sgsm.listen();
+      sgsm.print("\r");
+      delay(1000);
+      sgsm.print("AT+CMGF=1\r");
+      delay(1000);
+      /*Replace XXXXXXXXXX to 10 digit mobile number &
+        ZZ to 2 digit country code*/
+      sgsm.print("AT+CMGS=\"+916289605372\"\r");
+      delay(1000);
+      //The text of the message to be sent.
+      sgsm.print("https://www.google.com/maps/?q=");
+      sgsm.print(gpslat, 6);
+      sgsm.print(",");
+      sgsm.print(gpslon, 6);
+      delay(1000);
+      sgsm.write(0x1A);
+      delay(1000);
+      state = 1;
+    }
+  if (digitalRead(pin) == LOW) {
+      state = 0;
+    }
+  }
